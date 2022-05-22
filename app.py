8aef7fd1805952ddec87db258a7c280bea527b65
Oluwafemi Tairu <tairuoluwafemi09@gmail.com>
2022-05-22 09:32:19 +0100

    feat: updated pre-processor

--- app.py
+++ app.py
@@ -1,41 +1,38 @@
 import json
 import pickle
-from helper import vectorizer
+from pre_processor import input_parser
 
 classifier_folder_path = "model/movie_classifier.pkl"
 
 
 def lambda_handler(event, context):
     try:
-        query_params = event.get('queryStringParameters', {})
-        movie_review = query_params.get('movie_review', '')
+        query_params = event.get("queryStringParameters", {})
+        movie_review = query_params.get("movie_review", "")
         if not movie_review:
             return {
-                'statusCode': 401,
-                'body': json.dumps({
-                    "error": "1",
-                    "message": "Invalid parameter"
-                })
+                "statusCode": 401,
+                "body": json.dumps({"error": "1", "message": "Invalid parameter"}),
             }
         movie_review = [movie_review]
-        vectorized_review = vectorizer.transform(movie_review)
-        movie_classifier = pickle.load(open(classifier_folder_path, 'rb'))
-        prediction = movie_classifier.predict(vectorized_review)
-        review_sentiment = "Positive" if prediction[0] == 1 else "Negative"
-        return_data = {
-            "error": "0",
-            "message": "Successful",
-            "review_sentiment": review_sentiment
-        }
-        return {
-            'statusCode': 200,
-            'body': json.dumps(return_data)
-        }
+        is_successful, msg, return_data = input_parser(movie_review)
+        if is_successful:
+            movie_classifier = pickle.load(open(classifier_folder_path, "rb"))
+            prediction = movie_classifier.predict(return_data)
+            review_sentiment = "Positive" if prediction[0] == 1 else "Negative"
+            return_data = {
+                "error": "0",
+                "message": "Successful",
+                "data": review_sentiment,
+            }
+            return {"statusCode": 200, "body": json.dumps(return_data)}
+        else:
+            return {
+                "statusCode": 500,
+                "body": json.dumps({"error": "1", "message": msg}),
+            }
     except Exception as e:
         return {
-            'statusCode': 500,
-            'body': json.dumps({
-                "error": "1",
-                "message": str(e)
-            })
+            "statusCode": 500,
+            "body": json.dumps({"error": "1", "message": str(e)}),
         }
---
+++ pre_processor.py
@@ -0,0 +1,13 @@
+
+"""
+This is a sample python pre processing file for your model
+"""
+from typing import Tuple, Any
+
+def input_parser(data) -> Tuple[bool, str, Any]:
+	"""
+	do not change the name of this method
+	"""
+	# return the pre-processed version of your data here
+	return True, "Successful", data
+	
